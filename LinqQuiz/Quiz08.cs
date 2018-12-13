1abbcf08ddfadfe2ea016d1d59bdfbf50a0b9f00
sjhank <sj80812@gmail.com>
2018-12-13 13:55:26 +0800

    增加readme.txt，調整說明內容

--- LinqQuiz/Quiz08.cs
+++ LinqQuiz/Quiz08.cs
@@ -11,7 +11,7 @@ namespace LinqQuiz
     {
         public static void Run()
         {
-            // 請取出 employeeData 員工清單中，
+            // 請取出 employeeData 員工清單中(假設已依照 ID 排序)，第 6 ~ 10 名員工的資料
             // 進階題: 如果有一本員工名冊(依照 ID 排序)，一頁有五個員工，如何取出某一頁的員工資料
             // employeeData 內容請參考 Employees.cs (tip. 在下方 GetEmployeesDataTable 點一下，按下 F12)
 
---
+++ LinqQuiz/Readme.txt
@@ -0,0 +1,2 @@
+﻿
+請打開 Program.cs、QuizTemplate.cs 了解詳細內容，謝謝。
